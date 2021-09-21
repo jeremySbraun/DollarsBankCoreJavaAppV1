67e2930b51767664dbe27f9c14f5033cd381a672
Jeremy Braun <85400145+jeremySbraun@users.noreply.github.com>
2021-09-21 14:52:20 -0500

    Added all packages and classes

---
+++ DollarsBankCoreJavaAppV1/src/com/dollarsbank/model/Customer.java
@@ -0,0 +1,14 @@
+package com.dollarsbank.model;
+
+public class Customer {
+
+	
+	private String username;
+	private String password;
+	
+	private String fullName;
+	
+	private Account account;
+	
+	
+}
